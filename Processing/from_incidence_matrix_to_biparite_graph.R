9e72195583215d6a1e7dcf4fd4916419d991b7c9
Matthew J. Michalska-Smith <mjsmith037@gmail.com>
2019-01-25 10:52:00 -0600

    cleaning up

--- Processing/from_incidence_matrix_to_biparite_graph.R
+++
@@ -1,8 +0,0 @@
-library(igraph)
-
-from_incidence_matrix_to_bipartite <- function(tmp){
-  g <- graph_from_incidence_matrix(tmp)
-  cl <- clusters(g, "strong")
-  g2 <- induced.subgraph(g, which(cl$membership == which.max(cl$csize)))
-  return(g2)
-}
